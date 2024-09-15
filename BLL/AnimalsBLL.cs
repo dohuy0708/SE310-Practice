03abb549c154578d16abed1bebcba09581db52a7
dohuy0708 <dohuy22314@gmail.com>
2024-09-16 00:28:04 +0700

    Add project files.

---
+++ BLL/AnimalsBLL.cs
@@ -0,0 +1,45 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+
+namespace BLL
+{
+   public class AnimalsBLL
+    {
+        private AnimalsBLL() { }
+        private static AnimalsBLL instance;
+        public static AnimalsBLL Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AnimalsBLL();
+                }
+                return instance;
+            }
+
+        }
+        public int CountMilkCow()
+        {
+           int count = AnimalsAccess.Instance.CountMilkCow();
+            return count;
+        }
+        public int CountMilkSheep()
+        {
+            int count = AnimalsAccess.Instance.CountMilkGoat();
+            return count;
+        }
+        public int CountMilkGoat()
+        {
+            int count = AnimalsAccess.Instance.CountMilkSheep();
+            return count;
+        }
+    }
+}
---
+++ DAL/AnimalsAccess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+
+namespace DAL
+{
+   public class AnimalsAccess 
+    {
+        private AnimalsAccess() { }
+        private static AnimalsAccess instance;
+       public static AnimalsAccess Instance
+        {
+
+            get {
+                if ( instance == null)
+                {
+                    instance = new AnimalsAccess();
+                }
+                return instance; }
+             
+        }
+        public int CountMilkCow()
+        {
+            string query = "SELECT Milk FROM Animals WHERE Name = 'Cow'";
+
+            int MilkCow =SQLConnectionData.Instance.ExecuteQuery(query);
+            return MilkCow;
+        }
+
+        public int CountMilkSheep()
+        {
+            string query = "SELECT Num FROM Animals WHERE Name = 'Sheep'";
+
+            int MilkCow = SQLConnectionData.Instance.ExecuteQuery(query);
+            return MilkCow;
+        }
+
+        public int CountMilkGoat()
+        {
+            string query = "SELECT Num FROM Animals WHERE Name = 'Goat'";
+
+            int MilkCow = SQLConnectionData.Instance.ExecuteQuery(query);
+            return MilkCow;
+        }
+
+    }
+}
